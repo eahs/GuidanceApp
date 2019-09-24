4888e1fbef46eb0a9a0ad807404d77c1e5a8ab35
Jake Newman <jakenewman123456789@gmail.com>
2019-09-24 12:53:53 -0400

    Initial check-in of module Guidance1

---
+++ Guidance1/Views/MakeAppointment.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+
+namespace Guidance1.Views
+{
+    
+    public partial class MakeAppointment : ContentPage
+    {
+        public MakeAppointment()
+        {
+            
+        }
+        
+    }
+}
+
---
+++ Guidance1/Views/SAT.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Guidance1.Views
+{
+    public partial class SAT : ContentPage
+    {
+        public SAT()
+        {
+            InitializeComponent();
+        }
+    }
+}
