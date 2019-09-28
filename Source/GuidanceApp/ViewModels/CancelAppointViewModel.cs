45d4414252c1d2f6b3a43918fd4d5963ed1e0dba
Isaac Nunez <Isaacnunez2002@icloud.com>
2019-09-27 23:04:15 -0400

    Added U.I. for Appointment Cancellation page.

---
+++ Source/GuidanceApp/ViewModels/CancelAppointViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace GuidanceApp.ViewModels
+{
+    class CancelAppointViewModel : BaseViewModel
+    {
+        public CancelAppointViewModel ()
+        {
+            Title = "Cancel Appointment";
+        }
+
+    }
+}
