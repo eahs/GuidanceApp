36c79b8e949bb94781dfb2f2a19d3f48241c848e
Isaac Nunez <isaacnunez2002@icloud.com>
2019-10-31 11:28:03 -0400

    Changed Links Pages

---
+++ Source/GuidanceApp/ViewModels/ScholarshipViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+namespace GuidanceApp.ViewModels
+{
+    public class ScholarshipViewModel : BaseViewModel
+    {
+        public ScholarshipViewModel()
+        {
+            Title = "Scholarships";
+        }
+    }
+}
