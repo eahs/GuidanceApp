0f22c2bba15834757dfcff7d537bdff8d567373d
Josh <joshluscher@gmail.com>
2020-02-19 11:31:59 -0500

    Changed job board and schollar. page

---
+++ Source/GuidanceApp/Views/SearchBar.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using System.Collections.Generic;
+
+//[Xamarin.Forms.RenderWith(typeof(Xamarin.Forms.Platform._SearchBarRenderer))]
+//public class SearchBar : Xamarin.Forms.InputView,Xamarin.Forms.IElementConfiguration<Xamarin.Forms.SearchBar>,
+//Xamarin.Forms.Internals.IFontElement, Xamarin.Forms.ISearchBarController
