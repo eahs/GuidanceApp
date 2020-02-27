98d46683b1cc7b5702dbfe9f2dc05571252521da
Josh <joshluscher@gmail.com>
2020-02-27 11:19:49 -0500

    Made Global Construct

--- Source/GuidanceApp/GlobalConstants.cs
+++ Source/GuidanceApp/GlobalConstants.cs
@@ -3,8 +3,10 @@ namespace GuidanceApp
 {
     public class GlobalConstants
     {
-        public GlobalConstants()
-        {
-        }
+        // REST configuration
+        public static readonly string EndPointURL = "http://eahsguidance.me";
+        public static readonly int RequestTimeout = 10 * 1000; // In milliseconds, time to return request
+
+        public static readonly string ProductsEndPointRequestURL = "/api/GetProducts";
     }
 }
